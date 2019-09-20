aefc1ff9e56bdbc06ae93bae7099490f8089bcf2
pprabesh <prabes.pathak@gmail.com>
2019-09-20 16:50:34 -0400

    Deep RNN on time series data using keras

---
+++ rnn.py
@@ -0,0 +1,52 @@
+import sys
+assert sys.version_info >= (3, 5)
+
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow as tf
+from tensorflow import keras
+
+def generate_time_series(batch_size, n_steps):
+    freq1, freq2, offsets1, offsets2 = np.random.rand(4, batch_size, 1)
+    time = np.linspace(0, 1, n_steps)
+    series = 0.5 * np.sin((time - offsets1) * (freq1 * 10 + 10))  #   wave 1
+    series += 0.2 * np.sin((time - offsets2) * (freq2 * 20 + 20)) # + wave 2
+    series += 0.1 * (np.random.rand(batch_size, n_steps) - 0.5)   # + noise
+    return series[..., np.newaxis].astype(np.float32)
+
+n_steps = 50
+series = generate_time_series(10000, n_steps + 1)
+X_train, y_train = series[:7000, :n_steps], series[:7000, -1]
+X_valid, y_valid = series[7000:9000, :n_steps], series[7000:9000, -1]
+X_test, y_test = series[9000:, :n_steps], series[9000:, -1]
+
+
+""" Using Simple RNN
+
+model = keras.models.Sequential([keras.layers.SimpleRNN(1, input_shape=[None, 1])])
+
+"""
+
+"""Using DEEP RNN"""
+model = keras.models.Sequential([
+    keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+    keras.layers.SimpleRNN(20),
+    keras.layers.Dense(1)
+])
+
+
+#determine number of epochs
+no_epochs = 20
+
+#set error and optimizer
+model.compile(loss="mse", optimizer="adam")
+
+history = model.fit(X_train, y_train, epochs=no_epochs, validation_data=(X_valid, y_valid))
+evalu = model.evaluate(X_valid, y_valid)
+print(history)
+print(evalu)
+print(model.metrics_names, evalu)
